74b70c1792b4ac6cb08a43772fc2fa6ae9824a0e
大崔 <cuizhaohui527@gmail.com>
2018-10-12 15:04:23 +0800

    no message

---
+++ app/src/main/java/com/dc/countview/CountView.java
@@ -0,0 +1,31 @@
+package com.dc.countview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Create by CZH
+ * Create date 2018/10/12
+ * Description:
+ */
+public class CountView extends View {
+
+    public CountView(Context context) {
+        super(context);
+    }
+
+    public CountView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CountView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+
+    }
+
+}
+
